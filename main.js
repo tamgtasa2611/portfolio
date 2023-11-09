59808eae7c42404f768ce9d3c2aef879da133e01
tamgtasa2611 <97651251+tamgtasa2611@users.noreply.github.com>
2023-11-10 00:17:42 +0700

    css

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let aboutElement = document.getElementById("#about");
 let skillElement = document.getElementById("#skills");
 let projectElement = document.getElementById("#projects");
 let contactElement = document.getElementById("#contacts");
+let skillsItems = document.querySelectorAll(".skill-name");
 
 //add nav bar background when scroll down
 window.onscroll = function () {
@@ -40,21 +41,33 @@ var fadeRight = {
   origin: "right",
 };
 var fadeBottom = {
-  distance: "10%",
+  distance: "40%",
   origin: "bottom",
 };
 
 //ABOUT
 ScrollReveal().reveal(".about", fadeIn);
-ScrollReveal().reveal(".about", fadeLeft);
+ScrollReveal().reveal(".about", fadeBottom);
 //SKILLS
 ScrollReveal().reveal(".skills", fadeIn);
-ScrollReveal().reveal(".skills", fadeRight);
+ScrollReveal().reveal(".skills", fadeBottom);
 
 //PROJECTS
 ScrollReveal().reveal(".projects", fadeIn);
-ScrollReveal().reveal(".projects", fadeLeft);
+ScrollReveal().reveal(".projects", fadeBottom);
 
 //CONTACTS
 ScrollReveal().reveal(".contacts", fadeIn);
-ScrollReveal().reveal(".contacts", fadeRight);
+ScrollReveal().reveal(".contacts", fadeBottom);
+
+//skills item img
+skillsItems.forEach((item) => {
+  item.onmouseover = () => {
+    item.querySelector(".item-img").classList.add("visible");
+    item.querySelector(".item-img").classList.remove("hidden");
+  };
+  item.onmouseout = () => {
+    item.querySelector(".item-img").classList.remove("visible");
+    item.querySelector(".item-img").classList.add("hidden");
+  };
+});
